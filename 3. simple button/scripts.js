3085af77df9d1c7fae0dee3dd93929af486a37f4
Tomi79 <marecik@op.pl>
2019-12-11 23:14:12 +0100

    proof of concept działa dodawanie i usuwanie z koszyka

--- 3. simple button/scripts.js
+++ 3. simple button/scripts.js
@@ -2,47 +2,93 @@ window.onload = function f() {
 
     let mainFieldsElements = document.querySelector('#main-field');
 
-    let col = mainFieldsElements.addEventListener("click", addItemsToCart, false, );
-
+    mainFieldsElements.addEventListener("click", addItemsToCart, false, );
 
     function addItemsToCart(e) {
+
        let itemProperty = itemPropertyExtractor(e);
-        createItems(itemProperty[1], itemProperty[2]);
-        // console.log('itemProp ' + itemProp);
+       let itemName = itemProperty[1];
+       let itemPrice = itemProperty[2];
+       let itemIdNumber = itemProperty[0];
+
+        createItems(itemName, itemPrice, itemIdNumber);
+        addPrice(itemPrice);
+
     }
 
-    function itemPropertyExtractor(e) {
-        let property = [];
-        if (e.target !== e.currentTarget) {
-            property.push(e.target.id);
-            property.push(e.target.className);
-            property.push(e.target.value);
-            return property;
+        function itemPropertyExtractor(e) {
+            let property = [];
+            if (e.target !== e.currentTarget) {
+                property.push(e.target.id);
+                property.push(e.target.className);
+                property.push(e.target.value);
+                return property;
+            }
         }
-    }
 
-    function createItems(itemsClass, itemsValue ) {
-        this.tagName = 'div';
-        this.itemName = itemsClass;
-        this.itemPrice = itemsValue;
+        function createItems(itemsClass, itemsValue, itemIdNumber) {
+            this.tagName = 'div';
+            this.itemName = itemsClass;
+            this.itemPrice = itemsValue;
 
-        let description = document.createTextNode(this.itemName);
-        let nowyElement = document.createElement(this.tagName);   // Create a <button> element
-        nowyElement.appendChild(description);
+            let description = document.createTextNode(this.itemName);
+            let nowyElement = document.createElement(this.tagName);
+            nowyElement.id = itemIdNumber.toString() + '_cart';
 
-        let price = document.createTextNode(this.itemPrice);
-        let innerDiv = document.createElement(this.tagName);   // Create a <button> element
+            nowyElement.appendChild(description);
+            nowyElement.classList.add(itemIdNumber.toString());
 
-        innerDiv.appendChild(price);
-        innerDiv.classList.add('priceInBasket');
+            let price = document.createTextNode(this.itemPrice);
+            let innerDiv = document.createElement(this.tagName);
 
-        nowyElement.appendChild(innerDiv);
-        nowyElement.classList.add('itemsInBasket');
+            innerDiv.appendChild(price);
+            innerDiv.classList.add('priceInBasket');
 
-        let cart = document.getElementById('cart');
-        innerDiv.classList.add('cartWithItems');
-        cart.appendChild(nowyElement);
-    }
+            nowyElement.appendChild(innerDiv);
+            nowyElement.classList.add('itemsInBasket');
 
 
+            let cart = document.getElementById('cart');
+            innerDiv.classList.add('cartWithItems');
+            cart.appendChild(nowyElement);
+
+            let buttonDelete = document.createElement('button');
+            let buttonDeleteLabel = document.createTextNode('Usuń');
+            buttonDelete.appendChild(buttonDeleteLabel);
+            buttonDelete.id = itemIdNumber.toString() + '_db';
+            buttonDelete.value = this.itemPrice.toString();
+
+            nowyElement.appendChild(buttonDelete);
+
+            buttonDelete.addEventListener("click", deletItemFromCart, false,)
+            itemIdNumber ++;
+        }
+
+        let totalPrice = 0;
+        function addPrice(itemPrice) {
+            totalPrice += parseInt(itemPrice);
+            document.getElementById('total-price').textContent = totalPrice.toString();
+            console.log(totalPrice);
+        }
+
+        function deletItemFromCart(e) {
+
+            let objId = constructDeleteObjectId(e);
+            let usuwany = document.getElementById(objId);
+            usuwany.remove();
+            subtructPrice(e);
+
+        }
+
+        function constructDeleteObjectId(e) {
+            let buttonDeletId = e.target.id;
+            let itemId = buttonDeletId.split('_')[0].toString();
+            return itemId + '_ab_cart'
+        }
+
+        function subtructPrice(e) {
+            itemPrice = e.target.value;
+            totalPrice -= parseInt(itemPrice);
+            document.getElementById('total-price').textContent = totalPrice.toString();
+        }
 }
